e64f6f9e3914c25c354da5916aaa73aaee83cc2b
Jay Himanshu Jha <himanshujha19@gmail.com>
2020-06-07 21:29:05 -0700

    Fix toggle and update profile pic

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    if ($('.content-wrapper').hasClass('showing')){
+      $('.content-wrapper').removeClass('animated slideInRight')
+      $('.panel-cover').removeClass('panel-cover--collapsed')
+      $('.panel-cover').css('max-width', '100%')
+      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+      $('.content-wrapper').removeClass('showing')
+      window.location.hash = '';
+      parent.location.hash = ''
+      return;
+    }
+
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
@@ -15,6 +25,7 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
+    $('.content-wrapper').addClass('showing');
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
@@ -30,9 +41,9 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+  // $('.navigation-wrapper .blog-button').click(function () {
+  //   $('.navigation-wrapper').toggleClass('visible')
+  //   $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  // })
 
 })
